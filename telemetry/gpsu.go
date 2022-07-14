eb2e52f7808815d7b8803fb6f1585a69cb801256
Max8Curtis <52663084+Max8Curtis@users.noreply.github.com>
2022-07-14 17:54:27 +0100

    Revert to VM version

--- telemetry/gpsu.go
+++ telemetry/gpsu.go
@@ -1,26 +1,26 @@
 package telemetry
 
 import (
-	"errors"
-	"time"
+        "errors"
+        "time"
 )
 
 // GPS-acquired timestamp
 type GPSU struct {
-	Time time.Time
+        Time time.Time
 }
 
 func (gpsu *GPSU) Parse(bytes []byte) error {
-	if 16 != len(bytes) {
-		return errors.New("Invalid length GPSU packet")
-	}
+        if 16 != len(bytes) {
+        	return errors.New("Invalid length GPSU packet")
+        }
 
-	t, err := time.Parse("060102150405", string(bytes))
-	if err != nil {
-		return err
-	}
+        t, err := time.Parse("060102150405", string(bytes))
+        if err != nil {
+        	return err
+        }
 
-	gpsu.Time = t
+        gpsu.Time = t
 
-	return nil
+        return nil
 }
